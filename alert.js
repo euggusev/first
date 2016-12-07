6b25fe1d2f91bbfd60eab0556eb89320aa6a32c5
Eugene Gusev <euggusev@rambler.ru>
2016-12-07 11:14:52 +0300

    first commit

---
+++ alert.js
@@ -0,0 +1,11 @@
+﻿function line(x, y, x1, y1){
+    var example = document.getElementById("canvas"),
+    ctx = example.getContext('2d');
+    example.height = 200;
+    example.width  = 200;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x1, y1);
+    ctx.stroke();
+} 
+line(3, 10, 25, 50);
